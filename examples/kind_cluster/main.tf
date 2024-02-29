7f83d3359b8059d787f34e8efff39650a48dad2b
Sergio Marti <44024844+sermars@users.noreply.github.com>
2024-02-29 13:47:51 +0100

    makefiles and refactoring

--- examples/kind_cluster/main.tf
+++ examples/kind_cluster/main.tf
@@ -1,12 +1,12 @@
 module "local_k8s_cluster" {
-  source      = "../../modules/kind_cluster/"
-  
-  path_module = "../../modules/kind_cluster"
-  cluster_name = "ds-local-cluster"
+  source = "../../modules/kind_cluster/"
+
+  path_module  = "../../modules/kind_cluster"
+  cluster_name = var.cluster_name
 }
 
 module "portainerce_docker" {
-  source      = "../../modules/portainer_ce/"
+  source     = "../../modules/portainer_ce/"
   depends_on = [module.local_k8s_cluster]
 
   img_version = "2.19.4"
