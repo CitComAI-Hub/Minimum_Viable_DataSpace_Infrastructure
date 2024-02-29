7688eb8078ee13d3b159f375d44987fbca691f99
Sergio Marti <44024844+sermars@users.noreply.github.com>
2024-02-29 15:09:06 +0100

    extra_ports and extra_mounts added dynamically

--- modules/kind_cluster/variables.tf
+++ modules/kind_cluster/variables.tf
@@ -22,6 +22,39 @@ variable "kindest_version" {
   default     = "kindest/node:v1.29.2"
 }
 
+variable "add_extra_ports" {
+  description = "Extra ports to be added to control-plane node"
+  type = list(object({
+    container_port = number
+    host_port      = number
+    protocol       = string
+  }))
+
+  default = [
+    {
+      container_port = 80
+      host_port      = 80
+      protocol       = "TCP"
+    },
+    {
+      container_port = 443
+      host_port      = 443
+      protocol       = "TCP"
+    }
+  ]
+
+}
+
+variable "add_extra_mounts" {
+  description = "Extra mounts to be added to all nodes"
+  type = list(object({
+    host_path      = string
+    container_path = string
+  }))
+
+  default = []
+}
+
 variable "ingress_config_file" {
   type        = string
   description = "The path to the ingress config file"
