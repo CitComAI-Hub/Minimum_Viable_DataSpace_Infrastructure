b52e3647e622b23aa032e59abb0022a3cad27623
Sergio Marti <44024844+sermars@users.noreply.github.com>
2024-03-21 14:25:09 +0100

    refactoring to use kubectl and helm resources

--- modules/cert_trust_manager/main.tf
+++ modules/cert_trust_manager/main.tf
@@ -1,22 +1,40 @@
-locals {
-  cert_manager_repo = "https://github.com/cert-manager/cert-manager/releases/download/${var.cert_manager_version}/cert-manager.crds.yaml"
+data "http" "cert_manager_repo" {
+  url = "https://github.com/cert-manager/cert-manager/releases/download/${var.cert_manager_version}/cert-manager.crds.yaml"
 }
 
-resource "null_resource" "cert_manager" {
-  provisioner "local-exec" {
-    command = <<-EOF
-      helm repo add jetstack https://charts.jetstack.io --force-update && \
-      helm repo update && \
-      kubectl apply -f ${local.cert_manager_repo} && \
-      helm install cert-manager jetstack/cert-manager \
-        --namespace ${var.namespace} \
-        --create-namespace \
-        --version ${var.cert_manager_version} \
-        --wait && \
-      helm install trust-manager jetstack/trust-manager \
-        --namespace ${var.namespace} \
-        --version ${var.trust_manager_version} \
-        --wait
-    EOF
+data "kubectl_file_documents" "cert_manager_manifest" {
+  content = data.http.cert_manager_repo.response_body
+}
+
+resource "kubectl_manifest" "cert_manager" {
+  for_each  = data.kubectl_file_documents.cert_manager_manifest.manifests
+  yaml_body = each.value
+}
+
+resource "kubernetes_namespace" "cert_manager_namespace" {
+  metadata {
+    name = var.namespace
   }
+}
+
+resource "helm_release" "cert_manager" {
+  depends_on = [kubectl_manifest.cert_manager, kubernetes_namespace.cert_manager_namespace]
+  wait       = true
+
+  name       = "cert-manager"
+  namespace  = var.namespace
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = var.cert_manager_version
+}
+
+resource "helm_release" "trust_manager" {
+  depends_on = [helm_release.cert_manager]
+  wait       = true
+
+  name       = "trust-manager"
+  namespace  = var.namespace
+  repository = "https://charts.jetstack.io"
+  chart      = "trust-manager"
+  version    = var.trust_manager_version
 }
--- modules/cert_trust_manager/versions.tf
+++ modules/cert_trust_manager/versions.tf
@@ -1,8 +1,16 @@
 terraform {
   required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2.2"
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.26.0"
     }
   }
 }
