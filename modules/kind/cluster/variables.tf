9df4db577033ce7067c527456e232ac7b520bc04
Sergio Marti <44024844+sermars@users.noreply.github.com>
2024-03-21 13:06:36 +0100

    refactoring to use kubectl resources

--- modules/kind/cluster/variables.tf
+++ modules/kind/cluster/variables.tf
@@ -53,15 +53,3 @@ variable "add_extra_mounts" {
   description = "Extra mounts to be added to all nodes"
   default = []
 }
-
-variable "ingress_config_file" {
-  type        = string
-  description = "The path to the ingress config file"
-  default     = "https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml"
-}
-
-variable "loadbalancer_config_file" {
-  type        = string
-  description = "The path to the loadbalancer config file"
-  default     = "https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml"
-}
