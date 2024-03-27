7f4e0afe05ad5f68da3df37cc9f8bd2c694063d3
Sergio Marti <44024844+sermars@users.noreply.github.com>
2024-03-27 09:24:13 +0100

    added kubernetes version to ca_configuration

--- modules/ca_configuration/versions.tf
+++ modules/ca_configuration/versions.tf
@@ -4,5 +4,9 @@ terraform {
       source  = "hashicorp/null"
       version = "~> 3.2.2"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27.0"
+    }
   }
 }
