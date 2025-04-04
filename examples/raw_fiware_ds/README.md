20ce22dffe3578a1136d054229340eabf1a3a37c
Sergio Marti <44024844+sermars@users.noreply.github.com>
2025-04-04 19:46:18 +0200

    raw fiware data space example

---
+++ examples/raw_fiware_ds/README.md
@@ -0,0 +1,44 @@
+# Raw Fiware Data Space
+
+In this example, a Trust Anchor and a Data Space Consumer (Keycloak) are deployed on a Kubernetes (Kind) cluster. Both components (Trust Anchor and Keycloak) are deployed without any default configuration, which means:
+
+- **Trust Anchor:** has no DID registered.
+- **Keycloak:** has no client configured.
+
+## Trust Anchor
+
+List did registries:
+
+```bash
+> curl -s -X GET "http://172.18.255.200/v4/issuers" -H "Host: tir.ds-operator.local" | jq
+{
+  "title": "Internal Server Error",
+  "status": 500,
+  "detail": "Request could not be answered due to an unexpected internal error."
+}
+```
+
+## Consumer (Keycloak)
+
+Get issuer did:
+
+```bash
+> kubectl exec -it keycloak-0 -n consumer-a -- bash
+
+keycloak@keycloak-0:/$ cat /did-material/did.env 
+DID=did:key:zDnaenz8Uf2fZe5ym74mSDEXxDPwHhCt42wcgwGkvMfSH5GVg
+PROVIDER_DID=did:key:zDnaeT9u7vErBkfFi1ufAZprjsjjvKwEEWDvBNTqBKBo9NeNs
+```
+
+Web access:
+
+Keycloak can be used to issue VerifiableCredentials for users or services, to be used for authorization at other participants of the Data Space. It comes with 1 preconfigured users:
+
+- the `keycloak-admin`: has a password generated during deployment, it can be retrieved via:
+    ```bash
+    kubectl get secret -n consumer-a -o json issuance-secret | jq '.data."keycloak-admin"' -r | base64 --decode
+    ```
+
+Add `172.18.255.200     keycloak.consumer-a.local` to /etc/hosts.
+
+The admin-console of keycloak is available at: http://keycloak.consumer-a.local, login with the keycloak-admin
---
+++ examples/raw_fiware_ds/main.tf
@@ -0,0 +1,108 @@
+locals {
+  local_domain       = "local"
+  operator_namespace = "ds-operator"
+  consumer_namespace = "consumer-a"
+  provider_namespace = "provider-a"
+
+
+  operator_services_names = {
+    trust_anchor = "fiware-minimal-trust-anchor"
+    mysql        = "mysql"
+    til          = "trusted-issuers-list"
+    tir          = "trusted-issuers-registry"
+  }
+
+  provider_expose_services = {
+    apisix = true
+    # Below services are not exposed (ingress) by default (only for testing purposes)
+    ccs     = true
+    til     = true
+    did     = true
+    vcv     = true
+    pap     = true
+    scorpio = true
+    tmf_api = true
+    rainbow = true
+  }
+
+  provider_services_names = {
+    connector      = "fiware-data-space-connector"
+    mysql          = "mysql-db"
+    ccs            = "credentials-config-service"
+    til            = "trusted-issuers-list"
+    did            = "did-helper" # default name, not editable
+    vcv            = "vc-verifier"
+    postgresql     = "postgresql-db"
+    pap            = "pap-odrl"
+    apisix_service = "apisix-proxy"
+    apisix_api     = "apisix-api"
+    postgis        = "postgis-db"
+    scorpio        = "scorpio-broker"
+    tmf_api        = "tm-forum-api"
+    cm             = "contract-management"
+    rainbow        = "rainbow"
+    tpp_data       = "tpp-rainbow-data"
+    tpp_service    = "tpp-rainbow-service"
+    tpp_catalog    = "tpp-rainbow-catalog"
+  }
+
+}
+
+module "trust_anchor" {
+  source = "../../modules/fiware/trust_anchor/"
+
+  namespace      = local.operator_namespace
+  service_domain = "${local.operator_namespace}.${local.local_domain}"
+  services_names = local.operator_services_names
+
+  providers = {
+    kubernetes = kubernetes
+    helm       = helm
+  }
+}
+
+module "consumer" {
+  source = "../../modules/fiware/ds_connector/consumer/"
+
+  operator_namespace = local.operator_namespace
+  namespace          = local.consumer_namespace
+  service_domain     = "${local.consumer_namespace}.${local.local_domain}"
+
+  providers = {
+    kubernetes = kubernetes
+    helm       = helm
+  }
+
+  did = {
+    port         = 3001,
+    country      = "BE"
+    state        = "BRUSSELS"
+    locality     = "Brussels"
+    organization = "Fancy Marketplace Co."
+    common_name  = "www.fancy-marketplace.biz"
+  }
+}
+
+module "provider" {
+  source = "../../modules/fiware/ds_connector/provider/"
+
+  namespace      = local.provider_namespace
+  service_domain = "${local.provider_namespace}.${local.local_domain}"
+  services_names = local.provider_services_names
+  enable_ingress = local.provider_expose_services
+
+  providers = {
+    kubernetes = kubernetes
+    helm       = helm
+  }
+
+  # Services Configuration
+  did = {
+    port         = 3002,
+    country      = "DE"
+    state        = "SAXONY"
+    locality     = "Dresden"
+    organization = "M&P Operations Inc."
+    common_name  = "www.mp-operation.org"
+  }
+}
